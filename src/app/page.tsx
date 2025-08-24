a54304ab095bfcf393d888b77bb90a4c30da0156
Darl Floresca <floresca.darlellisong@gmail.com>
2025-08-24 14:58:44 +0800

    refactor(layout): remove data-theme attribute from RootLayout component
    refactor(page): update Home component structure and use dynamic import for ThemeToggle
    
    refactor(ThemeToggle): simplify component structure and improve theme toggle functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import ThemeToggle from "features/common/components/ThemeToggle";
+import dynamic from "next/dynamic";
+const ThemeToggle = dynamic(() => import("features/common/components/ThemeToggle"));
 
 /**
  * Home page component for the photobooth application.
  */
 export default function Home() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Photobooth</h1>
       <ThemeToggle />
-    </>
+    </div>
   );
 }
