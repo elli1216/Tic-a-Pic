cf5dae1227ffb7746148548a8220fb0b2d982d0d
Darl Floresca <floresca.darlellisong@gmail.com>
2025-09-01 17:04:55 +0800

    feat(success): add Success component to display a success message

---
+++ src/app/success/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = (): React.JSX.Element => {
+  return (
+    <div className='flex items-center justify-center p-4'>
+      <span className='font-semibold'>Success</span>
+    </div>
+  );
+};
+
+export default Success;
